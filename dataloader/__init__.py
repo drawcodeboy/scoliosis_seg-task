67ff2e83e31f261169cd23e6d9c73cdf955581cb
drawcodeboy <20rnjsekdns@naver.com>
2024-10-29 19:28:38 +0900

    [Modify] __init__.py load_dataset

--- dataloader/__init__.py
+++ dataloader/__init__.py
@@ -1,1 +1,6 @@
-from .__utils__ import dataset
+from .dataloader import ScoliosisDataset
+
+def dataset(dataset, data_dir, mode):
+    if dataset == "scoliosis":
+        # Scoliosis Dataset
+        return ScoliosisDataset(data_dir, mode, 'U-Net')
--- dataloader/__utils__.py
+++
@@ -1,6 +0,0 @@
-from .dataloader import ScoliosisDataset
-
-def dataset(dataset, data_dir, mode):
-    if dataset == "scoliosis":
-        # Scoliosis Dataset
-        return ScoliosisDataset(data_dir, mode, 'U-Net')

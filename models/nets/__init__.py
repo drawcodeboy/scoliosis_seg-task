f53e117662ecfbfe7afcd42d6689d74411763bd4
drawcodeboy <20rnjsekdns@naver.com>
2024-11-19 03:33:01 +0000

    [Update] preparing SegNeXt

--- models/nets/__init__.py
+++ models/nets/__init__.py
@@ -3,3 +3,5 @@ from .SegFormer.segformer import SegFormer
 def load_model(model_name='segformer', scale='b0', num_classes=1, in_chans=1):
     if model_name == 'segformer':
         return SegFormer(phi=scale, num_classes=num_classes, in_chans=in_chans)
+    elif model_name == 'segnext':
+        pass

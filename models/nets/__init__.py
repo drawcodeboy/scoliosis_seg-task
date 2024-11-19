f8975417b72a8ea9c2ccd200bbacd942b7f7e600
drawcodeboy <20rnjsekdns@naver.com>
2024-11-19 03:06:46 +0000

    [Update] model name argument

--- models/nets/__init__.py
+++ models/nets/__init__.py
@@ -1,5 +1,5 @@
 from .SegFormer.segformer import SegFormer
 
-def load_model(model_name='segformer', scale='b0', num_classes=1):
+def load_model(model_name='segformer', scale='b0', num_classes=1, in_chans=1):
     if model_name == 'segformer':
-        return SegFormer(phi=scale, num_classes=num_classes)
+        return SegFormer(phi=scale, num_classes=num_classes, in_chans=in_chans)
--- utils/save_ckpt.py
+++ utils/save_ckpt.py
@@ -3,13 +3,12 @@ import os
 import torch
 import numpy as np
 
-def save_model_ckpt(model, scale, current_epoch, dir, train_loss_type):
+def save_model_ckpt(model, model_name, current_epoch, dir):
     ckpt = {}
     ckpt['model'] = model.state_dict()
     ckpt['epochs'] = current_epoch
     
-    loss_type = 'unified_loss' if train_loss_type == True else 'dice_loss'
-    state_dict_name = f'SegFormer-{scale}-{current_epoch:03d}-{loss_type}.pth'
+    state_dict_name = f'{model_name}-{current_epoch:03d}.pth'
     
     try:
         torch.save(ckpt, os.path.join(dir, state_dict_name))
@@ -18,17 +17,16 @@ def save_model_ckpt(model, scale, current_epoch, dir, train_loss_type):
         print(f"Can\'t Save Model @epoch: {current_epoch}")
 
 
-def save_loss_ckpt(train_loss, val_loss, train_dir, val_dir, train_loss_type):
-    loss_type = 'unified_loss' if train_loss_type == True else 'dice_loss'
+def save_loss_ckpt(train_loss, val_loss, train_dir, val_dir, model_name):
     
     try:
-        np.save(os.path.join(train_dir, f'total_train_{loss_type}.npy'), np.array(train_loss))
+        np.save(os.path.join(train_dir, f'total_train_{model_name}.npy'), np.array(train_loss))
         print('Save Train Loss')
     except:
         print('Can\'t Save Train Loss')
     
     try:
-        np.save(os.path.join(val_dir, f'total_val_{loss_type}.npy'), np.array(val_loss))
+        np.save(os.path.join(val_dir, f'total_val_{model_name}.npy'), np.array(val_loss))
         print('Save Validation Loss')
     except:
         print('Can\'t Save Validation Loss')

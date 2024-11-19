7b6e79509bf9772205766cdde1c0c3ff4832d80b
drawcodeboy <20rnjsekdns@naver.com>
2024-11-19 03:33:28 +0000

    [Update] new function for delete previous epoch

--- utils/save_ckpt.py
+++ utils/save_ckpt.py
@@ -8,6 +8,26 @@ def save_model_ckpt(model, model_name, current_epoch, dir):
     ckpt['model'] = model.state_dict()
     ckpt['epochs'] = current_epoch
     
+    # 이전 Epoch를 제거하기 위함
+    prev_pths = os.listdir(dir)
+    
+    for prev_pth in prev_pths:
+        if not prev_pth.endswith(".pth"): # 확장자 파일이 아니면 건너뛰기
+            continue
+        
+        prev_parsed = prev_pth.split('-')
+        prev_model = prev_parsed[0]
+        prev_scale = prev_parsed[1]
+        prev_epoch = prev_parsed[2].split('.')[0]
+        
+        prev_epoch = int(prev_epoch)
+        
+        if (prev_model == model_name.split('-')[0]) and (prev_scale == model_name.split('-')[1]):
+            # 같은 모델에 같은 스케일만 제거
+            if prev_epoch < current_epoch:
+                # 현재 epoch 수보다 적으면 제거
+                os.remove(os.path.join(dir, prev_pth))
+    
     state_dict_name = f'{model_name}-{current_epoch:03d}.pth'
     
     try:
